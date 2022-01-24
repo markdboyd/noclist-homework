a26106a755c241e17696a9890fd516ceb3c054fc
Mark Boyd <markdavidboyd@gmail.com>
2022-01-23 22:31:56 -0500

    add JSDOC

--- request.js
+++ request.js
@@ -1,6 +1,13 @@
 const crypto = require('crypto');
 const http = require('http');
 
+/**
+ * Make an HTTP(S) request.
+ *
+ * @param {Object} requestParams
+ *   Request options passed to https://nodejs.org/api/http.html#httprequesturl-options-callback
+ * @returns {Promise<Object>} - Object containing response headers and body
+ */
 async function makeRequest(requestParams) {
   return new Promise((resolve, reject) => {
     const req = http.request(
@@ -22,6 +29,15 @@ async function makeRequest(requestParams) {
   });
 }
 
+/**
+ * Make an HTTP(S) request with built-in retry logic on request failure.
+ *
+ * @param {Object} requestParams
+ *   Request options passed to https://nodejs.org/api/http.html#httprequesturl-options-callback
+ * @param {number} [allowedRetries] - Number of times to retry request before throwing an error
+ * @returns {Promise<Object>} - Object containing response headers and body
+ * @throws {Error}
+ */
 async function makeRequestWithRetries(requestParams, allowedRetries = 3) {
   let currentRetry = 1;
   let response;
@@ -39,12 +55,25 @@ async function makeRequestWithRetries(requestParams, allowedRetries = 3) {
   }
 }
 
+/**
+ * Get auth token from server
+ * @param {Object} requestParams
+ *   Request options passed to https://nodejs.org/api/http.html#httprequesturl-options-callback
+ * @returns {Promise<string>} - Auth token from server
+ */
 async function getAuthToken(requestParams) {
   requestParams.method = 'HEAD';
   const { headers } = await makeRequestWithRetries(requestParams);
   return headers['badsec-authentication-token'];
 }
 
+/**
+ * Get user IDs from server
+ * @param {Object} requestParams
+ *   Request options passed to https://nodejs.org/api/http.html#httprequesturl-options-callback
+ * @param {string} authToken - auth token to use for request
+ * @returns {Promise<Array<string>>} - Array of user IDs
+ */
 async function getUserIds(requestParams, authToken) {
   requestParams.headers = requestParams.headers || {};
   requestParams.headers['X-Request-Checksum'] = crypto.createHash('sha256').update(`${authToken}/users`).digest('hex');
